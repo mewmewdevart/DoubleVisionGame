cbe270b145b622c971f02c4a097d4f7c9739587b
Larissa Cristina Benedito <mewmewdevart@gmail.com>
2023-12-06 11:54:42 -0300

    scripts(content) : add basic structure of the game

---
+++ script.js
@@ -0,0 +1,27 @@
+const imageSources = [
+	'./images/icon_00.png',
+	'./images/icon_01.png',
+	'./images/icon_02.png',
+	'./images/icon_03.png',
+	'./images/icon_04.png',
+  ];
+
+// Change the image/icon source on the start screen
+function changeIconStartScreen(imageNumber) {
+	const imageContainer = document.getElementById('imageDisplay');
+	imageContainer.innerHTML = `<img src="${imageSources[imageNumber]}" alt="Icon ${imageNumber}">`;
+	}
+	
+// Display each image for 5 seconds and then switch to the next on
+	let currentImage = 0;
+	function displayImages() {
+	changeIconStartScreen(currentImage);
+	
+	setTimeout(() => {
+		currentImage = (currentImage + 1) % imageSources.length;
+		displayImages();
+	}, 5000); 
+	}
+	
+// Start the image display
+displayImages();
